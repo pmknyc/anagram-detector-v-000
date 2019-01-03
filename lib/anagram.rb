9c4f8ff8dcc37acbdeb87892cd0f13aaf48c052b
Paula McKinley <pmknyc@optimum.net>
2019-01-03 01:49:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,12 @@ attr_accessor :word
   end
 
 
+# word given at initialize: split string into array of letters
+# sort letter array and keep word array as object
+# anagram_words array to match: 
+# split words array into individual arrays of letters in each word, sorted
+
+# ary <=> other_ary → -1, 0, +1 or nil
   def match(word_array)
     word = self.word.split.sort
     to_match = word_array.split(",")

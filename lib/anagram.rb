2b0ca41e80a9d600f19df926dbe4f38ed3ad780f
Paula McKinley <pmknyc@optimum.net>
2019-01-03 03:17:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,8 @@
+# word given at initialize: split string into array of letters
+# sort letter array and keep word array as object
+# anagram_words array to match:
+# split words array into individual arrays of letters in each word, sorted
+# or send words to hash with orig_word => sorted_word
 class Anagram
 attr_accessor :word
 
@@ -5,21 +10,14 @@ attr_accessor :word
     @word = word
   end
 
-
-# word given at initialize: split string into array of letters
-# sort letter array and keep word array as object
-# anagram_words array to match:
-# split words array into individual arrays of letters in each word, sorted
-# or send words to hash with orig_word => sorted_word
-
-# ary <=> other_ary → -1, 0, +1 or nil
-# or use anagram_words.include?(word) to match word array to each sorted
-  def match(word_array)
+	def match(word_array)
     anagram = self.word.split(//).sort
-    matches = {}
-    word_array.each do |word|
-      matches[word] = word.split(//).sort
-    end
-    matches
-  end
-end
+    matched = []
+		match_index = nil
+		  word_array.each_with_index do |word, index|
+      	matched = word.split(//).sort
+				match_index = index if matched == anagram
+			end 
+		word_array[match_index] if match_index != nil
+	end
+end		

a918f617bd36e0fdb2d6c5ae0cfc82c90231bac6
Paula McKinley <pmknyc@optimum.net>
2019-01-03 01:40:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(word_array)
+    word = self.word.split.sort
+    to_match = word_array.split(",")
+
+  end
+end
